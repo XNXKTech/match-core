622cb628c2b44be0815849915de33a42fe517a18
Teakowa Gatanothor O'deorain <hub+git@teakowa.dev>
2021-02-26 15:35:15 +0800

    feat: init actix-web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{middleware, web, App, HttpRequest, HttpServer};
+
+async fn index(req: HttpRequest) -> &'static str {
+    println!("REQ: {:?}", req);
+    "Hello world!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .service(web::resource("/").to(index))
+    })
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
